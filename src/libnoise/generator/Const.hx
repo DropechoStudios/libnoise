7eb7ff71dc331053e00f62ed61dcaadb401c7602
Benjamin Van Treese <vantreeseba@gmail.com>
2016-03-11 01:54:00 -0600

    Set up for node module
    Add expose to all classes
    Add helper files, (.gitignore,.travis.yml,.npmignore)
    Add js target

---
+++ src/libnoise/generator/Const.hx
@@ -0,0 +1,16 @@
+package libnoise.generator;
+
+@:expose
+class Const extends ModuleBase{
+
+	var value : Int;
+
+	public function new(value : Int) {
+		super(0);
+		this.value = value;
+	}
+
+	override public function getValue(x : Float, y : Float, z : Float) : Float{
+		return value;
+	}
+}
